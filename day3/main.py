25a6b8590c3b7e8a980b577f8e2e2b95b542ad14
DatAmstrongest <93247554+DatAmstrongest@users.noreply.github.com>
2025-12-03 09:04:29 +0100

    Day3 part2 is completed

--- day3/main.py
+++ day3/main.py
@@ -10,8 +10,30 @@ def part1(lines):
                 biggest_digit = digit
         total += biggest_voltage
     return total
-def part2():
-    return
+
+def part2(lines):
+    total = 0
+    for line in lines:
+        voltage_array = []
+        for i in range(len(line)):
+            digit = line[i]
+            discards = len(line) - i - (12 - len(voltage_array))
+            if len(voltage_array) == 0 :
+                voltage_array.append(digit)
+            elif int(digit) >= int(voltage_array[-1]) and discards > 0:
+                while len(voltage_array) > 0 and  int(digit) > int(voltage_array[-1]) and discards > 0:
+                    voltage_array.pop()
+                    discards -= 1
+                if len(voltage_array) < 12:
+                    voltage_array.append(digit)
+            elif len(voltage_array) < 12:
+                voltage_array.append(digit)
+        total += int("".join(voltage_array))
+
+    return total
+
+
+
 
 def read_data():
     lines = []
@@ -21,4 +43,5 @@ def read_data():
     return lines
 
 lines = read_data()
-print(part1(lines))
+print(f"Part1 answer: {part1(lines)}")
+print(f"Part2 answer: {part2(lines)}")
