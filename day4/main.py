40eb93996e3af95fe891bcdd5b3d005796533954
DatAmstrongest <93247554+DatAmstrongest@users.noreply.github.com>
2025-12-04 14:27:26 +0100

    Part 1 is completed

---
+++ day4/main.py
@@ -0,0 +1,40 @@
+def is_roll(r, c, matrix):
+    if r<0 or r>=len(matrix) or c<0 or c>=len(matrix[0]):
+        return 0
+    return  matrix[r][c] == "@"
+
+def is_accessible(r, c, matrix):
+    counter = is_roll(r-1, c, matrix) \
+        + is_roll(r+1, c, matrix) \
+        + is_roll(r, c+1, matrix) \
+        + is_roll(r, c-1, matrix) \
+        + is_roll(r-1, c+1, matrix) \
+        + is_roll(r-1, c-1, matrix) \
+        + is_roll(r+1, c+1, matrix) \
+        + is_roll(r+1, c-1, matrix)
+    return counter < 4
+
+def read_file():
+    matrix = []
+    with open("data.txt") as file:
+        for line in file.readlines():
+            matrix.append(list(line.strip()))
+
+    return matrix
+
+
+
+
+def part1(matrix):
+    answer = 0
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if matrix[r][c] == "@" and is_accessible(r, c, matrix):
+                answer += 1
+    return answer
+
+if __name__ == "__main__":
+    matrix = read_file()
+    
+    print(f"Part1 Answer: {part1(matrix)}")
+
