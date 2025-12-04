ce679f10f89b2f2048553008adc5fc07a079783e
DatAmstrongest <93247554+DatAmstrongest@users.noreply.github.com>
2025-12-04 14:36:06 +0100

    Part2 is completed

--- day4/main.py
+++ day4/main.py
@@ -33,8 +33,23 @@ def part1(matrix):
                 answer += 1
     return answer
 
+def part2(matrix):
+    prev_answer = -1
+    answer = 0
+    while (prev_answer != answer):
+        prev_answer = answer
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == "@" and is_accessible(r, c, matrix):
+                    matrix[r][c] = "."
+                    answer += 1
+        
+    return answer
+
+
 if __name__ == "__main__":
     matrix = read_file()
     
     print(f"Part1 Answer: {part1(matrix)}")
+    print(f"Part2 Answer: {part2(matrix)}")
 
