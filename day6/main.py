83f85229429b1ca8e56eb20bd2d11c8fc55795fb
DatAmstrongest <93247554+DatAmstrongest@users.noreply.github.com>
2025-12-06 13:30:01 +0100

    Day6 Part2 is completed

--- day6/main.py
+++ day6/main.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def part1(nums, operations):
     total_sum = 0
     for i in range(len(operations)):
@@ -17,26 +19,57 @@ def part1(nums, operations):
         total_sum += local_sum
     return total_sum
 
-def part2():
-    return
+def part2(lines):
+    total_sum = 0
+    nums = []
+    pass_next = False
+    for c in range(len(lines[0])-1,-1,-1):
+        str_num = ""
+        if pass_next:
+            pass_next = False
+            continue
+        for r in range(len(lines)):
+            if r == len(lines)-1:
+                if r!="":
+                    nums.append(int(str_num))
+                    if lines[r][c]=="*":
+                        total_sum += reduce(lambda x, y: x*y, nums)
+                        pass_next = True
+                        nums = []
+                    if lines[r][c]=="+":
+                        total_sum += reduce(lambda x, y: x+y, nums)
+                        pass_next = True
+                        nums = []
+            else:
+                if lines[r][c] == " ":
+                    str_num += ""
+                else:
+                    str_num += lines[r][c]
+
+    return total_sum
+
+            
+
 
 def read_file(file_name):
     nums = []
     operations = []
+    lines = []
 
     with open(file_name) as file:
         for line in file.readlines():
+            lines.append(line.strip("\n"))
             line = line.strip()
             if (line[0].isdigit()):
                 nums.append([int(num) for num in line.split(" ") if num.isdigit()])
             else:
                 operations = [operation for operation in line.strip().split(" ") if operation!='']
-    return nums, operations
+    return nums, operations, lines
 
 
 if __name__ == "__main__":
-    nums, operations = read_file("data.txt")
-
+    nums, operations, lines = read_file("data.txt")
     print(f"Part1 Answer: {part1(nums, operations)}")
+    print(f"Part2 Answer: {part2(lines)}")
 
     
